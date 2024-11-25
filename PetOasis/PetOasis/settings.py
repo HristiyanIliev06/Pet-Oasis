25945e104b592874c058f788ea0087a9b2c98d95
unknown <hristiyan2006@gmail.com>
2024-11-26 01:18:05 +0200

    More changes

--- PetOasis/PetOasis/settings.py
+++ PetOasis/PetOasis/settings.py
@@ -131,7 +131,12 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR/'static',]
 
+MEDIA_URL = 'media/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.AppUser'
